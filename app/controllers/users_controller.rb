2951d60c23618210ab105f833f2827f0c93ccf1f
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-26 14:17:54 -0700

    jwt gem, authentication and login for users, and a sessions controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def index
+    user = current_user
+    render json: user.as_json
+  end
+end
